64d66babe19876ac8bba811e56ea55da22ca14d0
Donte Ladatto <donte279@gmail.com>
2022-10-21 12:15:12 -0500

    commit tree 9250b3e0de5da3591bb92e9cc46ef2bbd54c6894

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,32 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+  def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+  end
+
+  def add(v)
+    if @s1.empty?
+      @s1.push(v)
+    else
+      @s2.push(v)
+    end
+  end
+
+  def remove
+    if @s1.empty?
+      until @s2.empty? do
+        @s1.push(@s2.pop)
+      end
+    end
+    @s1.pop
+  end
+
+  def peek
+    if @s1.empty?
+      @s2.peek
+    end
+    @s1.peek
+  end
+end
